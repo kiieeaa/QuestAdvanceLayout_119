29399452d889961dcb100cea91a8e6193f0509b2
kiieeaa <kiyaadinda5@gmail.com>
2025-10-23 22:03:55 +0700

    menambahkan Jarak antara gambar dan tulisan

--- app/src/main/java/com/example/prak3/UIpertama.kt
+++ app/src/main/java/com/example/prak3/UIpertama.kt
@@ -96,6 +96,7 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
 
         }
         Box(
